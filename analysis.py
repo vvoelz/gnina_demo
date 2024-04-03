48865a6477cac9d0f72486b317c1b39ddc8e10dc
Vincent Voelz <vvoelz@gmail.com>
2024-04-02 22:54:06 -0400

    analyze all

--- analysis.py
+++ analysis.py
@@ -1,4 +1,7 @@
 import os, sys
+import subprocess
+import rdkit
+from rdkit import Chem
 
 # Helper function(s)
 
@@ -8,7 +11,6 @@ def run_cmd(cmd, testing=False):
     OPTIONS
     testing          If True, just print the cmd. Default: False
     """
-
     print('>>', cmd)
     if not testing:
         os.system(cmd)
@@ -25,83 +27,85 @@ if not os.path.exists('./DockRMSD'):
 # ...convert the xtal structure pose to mol2
 run_cmd('obabel input_files/donepezil_xtal.pdb  -O input_files/donepezil_xtal.mol2 --ff GAFF')
 
-# ... and convert # convert our docking poses to mol2
-run_cmd('obabel docking_results/smina_donepezil.sdf -O docking_results/smina_donepezil.mol2 -m')
-#run_cmd('obabel docking_results/gnina_rescore_donepezil.sdf -O docking_results/gnina_rescore_donepezil.mol2 -m')
-#run_cmd('obabel docking_results/gnina_refinement_donepezil.sdf -O docking_results/gnina_refinement_donepezil.mol2 -m')
+methods = ['smina', 'gnina_rescore', 'gnina_refinement']
+results = {} # store results in a dictionary
 
-for ligand in ['donepezil', 'galantamine', 'huperzine', 'rivastigmine', 'AC6']:
+for method in methods:
+
+  # ... and convert # convert our docking poses to mol2
+  run_cmd(f'obabel docking_results/smina_donepezil.sdf -O docking_results/{method}_donepezil.mol2 -m')
+
+  for ligand in ['donepezil', 'galantamine', 'huperzine', 'rivastigmine', 'AC6']:
+
+    resultname = f'{method}_{ligand}'
+    results[resultname] = {'minimizedAffinity':[], 'CNNscore':[], 'CNNaffinity':[], 'RMSD-to-xtal':[]}
+
+    # Load the SDF file
+    sdf_file = Chem.SDMolSupplier(f'docking_results/{resultname}.sdf')
+
+    # Iterate over the molecules in the file
+    for mol in sdf_file:
+
+        # get the smina affinity
+        minimizedAffinity = mol.GetProp('minimizedAffinity')
+        results[resultname]['minimizedAffinity'].append(float(minimizedAffinity))
+
+        # get the gnina CNN affinities
+        if method == 'smina':
+            results[resultname]['CNNscore'].append(None)
+            results[resultname]['CNNaffinity'].append(None)
+        else:
+            CNNscore = mol.GetProp('CNNscore')
+            CNNaffinity = mol.GetProp('CNNaffinity')
+            results[resultname]['CNNscore'].append(CNNscore)
+            results[resultname]['CNNaffinity'].append(CNNaffinity)
+
+        # fill RMSD-to-xtal fields with Nones
+        results[resultname]['RMSD-to-xtal'].append(None)
 
 
-#############
-### ANALYSIS: docking score versus RMSD-to-xtal
+    # If the ligand is donepezil, fill the RMSD-to-xtal fields
+    if ligand == 'donepezil':
 
-# resultname = 'smina_donepezil'
-resultname = 'gnina_rescore_donepezil'
-# resultname = 'gnina_refinement_donepezil'
+        # To get the rmsd-to-xtal of the docked pose, we need to parse the output of DockRMSD
+        posed_mol2files = [f'docking_results/{resultname}{i}.mol2' for i in range(1,10)]
+        for j in range(len(posed_mol2files)):
 
-# To get the docking score(s), we need to parse the 'docking_results/gnina_donepezil.sdf' file using rdkit
+              # Create a subprocess to run the 'ls' command
+              xtal_mol2 = 'input_files/donepezil_xtal.mol2'
+              output = subprocess.check_output(['./DockRMSD', xtal_mol2, posed_mol2files[j]], text=True)
 
-import rdkit
-from rdkit import Chem
+              # parse the RMSD from the line "Calculated Docking RMSD: ##.###\n"
+              lines = [line for line in output.split('\n') if line.count("Calculated Docking RMSD:") > 0]
+              rmsd = float(lines[0].strip().split()[-1])
+              print('rmsd', rmsd)
+              results[resultname]['RMSD-to-xtal'][j] = rmsd
 
-# Load the SDF file
-sdf_file = Chem.SDMolSupplier(f'docking_results/{resultname}.sdf')
 
-minimizedAffinity_values = []
-CNNscore_values = []
-CNNaffinity_values = []
-
-# Iterate over the molecules in the file
-for mol in sdf_file:
-
-    # get the smina affinity
-    minimizedAffinity = mol.GetProp('minimizedAffinity')
-    minimizedAffinity_values.append(float(minimizedAffinity))
-    print('minimizedAffinity', minimizedAffinity, end=' ')
-
-    # get the gnina CNN affinities
-    if resultname.count('smina') == 0:
-      CNNscore = mol.GetProp('CNNscore')
-      CNNaffinity = mol.GetProp('CNNaffinity')
-      CNNscore_values.append(float(CNNscore))
-      CNNaffinity_values.append(float(CNNaffinity))
-      print('CNNscore', CNNscore, 'CNNaffinity', CNNaffinity, end='')
+    # PRINT the results
+    print('-------------')
     print()
+    print(resultname)
+    num_poses = 9
+    if method == 'smina':
 
-import subprocess
+      if ligand == 'donepezil':
+          print('pose, RMSD-to-xtal (A), minimizedAffinity (kcal/mol)')
+          for j in range(num_poses):
+              print(f"{j+1}, {results[resultname]['RMSD-to-xtal'][j]}, {results[resultname]['minimizedAffinity'][j]}")
+      else:
+          print('pose, minimizedAffinity (kcal/mol)')
+          for j in range(num_poses):
+              print(f"{j+1}, {results[resultname]['minimizedAffinity'][j]}")
 
-# resultname = 'smina_donepezil'
-resultname = 'gnina_rescore_donepezil'
-# resultname = 'gnina_refinement_donepezil'
+    else:
 
-# To get the rmsd-to-xtal of the docked pose, we need to parse the output of DockRMSD
-posed_mol2files = [f'docking_results/{resultname}{i}.mol2' for i in range(1,10)]
-rmsd_values = []
-for posed_mol2file in posed_mol2files:
-
-    try:
-      # Create a subprocess to run the 'ls' command
-      xtal_mol2 = 'input_files/donepezil_xtal.mol2'
-      output = subprocess.check_output(['./DockRMSD', xtal_mol2, posed_mol2file], text=True)
-
-      # parse the RMSD from the line "Calculated Docking RMSD: ##.###\n"
-      lines = [line for line in output.split('\n') if line.count("Calculated Docking RMSD:") > 0]
-      rmsd = float(lines[0].strip().split()[-1])
-      rmsd_values.append(rmsd)
-    except:
-      rmsd_values.append(None)
-
-print('rmsd_values', rmsd_values)
-print()
-
-if resultname.count('smina') > 0:
-    print('RMSD-to-xtal (A), minimizedAffinity_values (kcal/mol)')
-    for i in range(len(minimizedAffinity_values)):
-        print(f'pose {i}: {rmsd_values[i]},  {minimizedAffinity_values[i]}')
-
-else:
-    print('RMSD-to-xtal (A), CNNaffinity (pKd)')
-    for i in range(len(minimizedAffinity_values)):
-        print(f'pose {i}: {rmsd_values[i]},  {CNNaffinity_values[i]}')
+      if ligand == 'donepezil':
+          print('pose, RMSD-to-xtal (A), CNNaffinity (pKd)')
+          for j in range(num_poses):
+              print(f"{j+1}, {results[resultname]['RMSD-to-xtal'][j]}, {results[resultname]['CNNaffinity'][j]}")
+      else:
+          print('pose, CNNaffinity (pKd)')
+          for j in range(num_poses):
+              print(f"{j+1}, {results[resultname]['CNNaffinity'][j]}")
 
